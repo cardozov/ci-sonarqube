24c8acc81d95e6d2f9bf0b2bc20c0a693c4abb8e
cardozov <vinicius.gcardozo@gmail.com>
2023-03-25 07:06:06 -0300

    removing tests to trigger Quality Check Error

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -2,30 +2,6 @@ const request = require('supertest')
 const server = require('../index')
 
 describe('Get Endpoints', () => {
-  it('should return 200 with body from /', async () => {
-    const res = await request(server).get('/')
-    expect(res.statusCode).toEqual(200)
-    expect(res.body).toHaveProperty('nome')
-  })
-
-  it('should return 200 with body from /health', async () => {
-    const res = await request(server).get('/health')
-    expect(res.statusCode).toEqual(200)
-    expect(res.body).toHaveProperty('status')
-  })
-
-  it('should return 500 with body from /error', async () => {
-    const res = await request(server).get('/error')
-    expect(res.statusCode).toEqual(500)
-    expect(res.body).toHaveProperty('status')
-  })
-
-  it('should return 200 with body from /timeout', async () => {
-    const res = await request(server).get('/timeout')
-    expect(res.statusCode).toEqual(200)
-    expect(res.body).toHaveProperty('status')
-  }, 2500)
-
   it('should return 404 from /notfound', async () => {
     const res = await request(server).get('/notfound')
     expect(res.statusCode).toEqual(404)

f6be49003387c1bf80956fdf15a007b2e385f204
cardozov <vinicius.gcardozo@gmail.com>
2023-03-25 05:11:55 -0300

    sonar: fixing OWASP code smell - javascript:S5689

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const helmet = require('helmet')
+
 const app = express()
+app.use(helmet())
 const port = process.env.PORT || 3003
 
 app.get('/', (req, res) => {
